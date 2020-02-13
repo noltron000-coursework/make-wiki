fce23e253e0a23dfa99a1bc97003c31954f74602
Nolan Kovacik <noltron000@gmail.com>
2020-02-12 16:55:55 -0800

    [fmt] redo challenges.md

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,8 +3,8 @@ from wiki.models import Page
 
 
 class PageAdmin(admin.ModelAdmin):
-    """ Show helpful fields on the changelist page. """
-    list_display = ('title', 'slug', 'author', 'created', 'modified')
+	""" Show helpful fields on the changelist page. """
+	list_display = ('title', 'slug', 'author', 'created', 'modified')
 
 
 admin.site.register(Page, PageAdmin)
--- wiki/forms.py
+++ wiki/forms.py
@@ -3,5 +3,5 @@ from wiki.models import Page
 
 
 class PageForm(forms.ModelForm):
-    """ Render and process a form based on the Page model. """
-    model = Page
+	""" Render and process a form based on the Page model. """
+	model = Page
