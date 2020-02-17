20319e3a0194258b532975a9bb9d85c340111f0c
Nolan Kovacik <noltron000@gmail.com>
2020-02-17 13:19:23 -0800

    [add] challenge progress

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,29 +1,28 @@
 from django.urls import path
-'''
-== HINT ==
-from wiki.views import PageListView, PageDetailView
-'''
+from wiki.views import PageList, PageDetail
 
 '''
-== CHALLENGES ==
-1. Refactor the URL named `wiki-list-page` and point it to the root route (`/`).
-	- Make sure Django doesn't give you any warnings or errors when you execute `python manage.py runserver`.
-	- Test by visiting http://127.0.0.1:8000/.
-2. Refactor the URL named `wiki-details-page` to show the DetailsView for any Page that exists.
-	- Use the slug field in the Page model to accomplish this.
-	- DO NOT CHANGE the `name` argument.
-	- Test by visiting http://127.0.0.1:8000/w/title-but-replace-spaces-with-dashes in your browser.
+==CHALLENGES==
+1. Refactor the URL named `wiki-list-page`.
+	-> Point it to the root route (`/`).
+	-> No warnings or errors after running:
+		`python manage.py runserver`
+	-> Test by visiting:
+		`http://127.0.0.1:8000/`
+
+2. Refactor the URL named `wiki-details-page`.
+	-> Show the DetailsView(?) for any page that exists.
+	-> Use the slug field in the Page(?) model to do this.
+	-> DO NOT CHANGE the `name` argument.
+	-> Test by visiting:
+		`http://127.0.0.1:8000/w/{wiki-page-title}`
 '''
 
+# ==TODO==
+# Check documentation on these things:
+# -> '<str:slug>/' string
+# -> 'as_view()' method
 urlpatterns = [
-	# path('REPLACE_ME_WITH_ROOT_ROUTE', REPLACE_ME_WITH_VIEW.as_view(), name='wiki-list-page'),
-	# path('REPLACE_ME_WITH_SLUG', REPLACE_ME_WITH_VIEW.as_view(), name='wiki-details-page'),
+	path('', PageList.as_view(), name='wiki-list-page'),
+	path('<str:slug>/', PageDetail.as_view(), name='wiki-details-page'),
 ]
-
-'''
-== HINT ==
-urlpatterns = [
-	path('', PageListView.as_view(), name='wiki-list-page'),
-	path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
-]
-'''
--- wiki/views.py
+++ wiki/views.py
@@ -8,49 +8,54 @@ class PageList(ListView):
 	'''
 	Renders a list of all Pages.
 
-	== CHALLENGES ==
-	- On GET, display a homepage that shows all Pages in your wiki.
-	- Replace this CHALLENGE text with a descriptive docstring for PageList.
-	- Replace pass below with the code to render a template named `list.html`.
+	==CHALLENGES==
+	1. GET: Have a homepage showing all Pages in your wiki.
+	2. Add a descriptive docstring for PageList.
+	3. Replace `pass` below with your code.
+		-> Render a template named `list.html`.
 	'''
 	model = Page
 
 	def get(self, request):
 		'''
-		Returns a list of wiki pages.
+		GET Pages
+		-> Returns a list of wiki pages.
 		'''
-		pass
-
-	"""
-	== HINT ==
-	def get(self, request):
-		'''
-		GET a list of Pages.
-		'''
+		# == TODO ==
+		# -> Get more intimate with .get_queryset() and .all()
+		# -> Get more familiar with render(), and its params
+		# -> Understand what the request param is
 		pages = self.get_queryset().all()
 		return render(request, 'list.html', {
 			'pages': pages
 		})
-	"""
 
-class PageDetailView(DetailView):
+class PageDetail(DetailView):
 	'''
 	Renders a specific page based on it's slug.
 	
-	== CHALLENGES ==
-	- On GET, render a template named `page.html`.
-	- Replace this docstring with a description of what thos accomplishes.
+	==CHALLENGES==
+	1. GET: Have a homepage showing the details of your page.
+	2. Add a descriptive docstring for PageDetail.
+	3. Replace `pass` below with your code.
+		-> Render a template named `page.html`.
 
-	== STRETCHES ==
-	- Import the PageForm class from forms.py.
-		- This ModelForm enables editing of an existing Page object in the database.
-	- On GET, render an edit form below the page details.
-	- On POST, check if the data in the form is valid.
-		- If True, save the data, and redirect back to the DetailsView.
-		- If False, display all the errors in the template, above the form fields.
-	- Instead of hard-coding the path to redirect to, use the `reverse` function to return the path.
-	- After successfully editing a Page, use Django Messages to "flash" the user a success message
-		- Message Content: REPLACE_WITH_PAGE_TITLE has been successfully updated.
+	==STRETCHES==
+	1. Import the PageForm class from forms.py.
+		-> This ModelForm allows edits in existing Page objects.
+	2. GET: Render an edit form below the page details.
+	3. POST: Check if the data in the form is valid.
+		-> If True, save the data. 
+			Redirect back to the DetailsView.
+		-> If False, display all the errors in the template.
+			Do not redirect; display above the form fields.
+
+	==NOTE==
+	-> After successfully editing a Page: 
+		-> Use Django Messages to "flash" a success message.
+			`{PAGE_TITLE} has been successfully updated.`
+	-> Don't hard-coding the path to redirect to.
+		-> Use the `reverse` function to return the path instead.
 	'''
 	model = Page
 
@@ -58,19 +63,15 @@ class PageDetailView(DetailView):
 		'''
 		Returns a specific of wiki page by slug.
 		'''
-		pass
-
-	"""
-	== HINT ==
-	def get(self, request, slug):
-		'''
-		Returns a specific wiki page by slug.
-		'''
+		# == TODO ==
+		# -> Get more intimate with .get_queryset() and .get()
+		# -> Get more familiar with render(), and its params
+		# -> Understand the slug__iexact param
+		# -> Understand what the request param is
 		page = self.get_queryset().get(slug__iexact=slug)
 		return render(request, 'page.html', {
 			'page': page
 		})
-	"""
 
 	def post(self, request, slug):
 		'''
